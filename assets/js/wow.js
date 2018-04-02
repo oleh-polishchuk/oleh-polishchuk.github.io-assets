5f0fc63e536fde7512d56db6bae0c7d4e0062490
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2018-04-02 21:47:25 +0300

    Add js assets

---
+++ assets/js/wow.js
@@ -0,0 +1,11 @@
+/**
+ * Reveal CSS animation as you scroll down a page http://mynameismatthieu.com/WOW/
+ *
+ * @author https://github.com/matthieua/WOW
+ */
+
+export function init() {
+  return (function () {
+    new WOW().init();
+  })();
+}
